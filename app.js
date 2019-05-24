618a44b39149614fe12c2959d4c2864b42353af5
7mioda <ahmededaly1993@gmail.com>
2019-05-24 11:28:31 +0200

    adding themes to portfolio

--- app.js
+++ app.js
@@ -64,10 +64,78 @@ const scroll = new SmoothScroll('a[href*="#"]', { speed: 800 });
 //--------------------------------------------------------------
 //                          /* Up button */
 //--------------------------------------------------------------
-
-window.addEventListener('scroll' , (event)=> {
-    console.log(window.pageYOffset || document.documentElement.scrollTop);
+const upBtn = document.querySelector('.up-btn');
+const intro = document.querySelector('#intro');
+upBtn.addEventListener('click',(event)=> {
+    scroll.animateScroll(intro);
 });
+window.addEventListener('scroll' , (event)=> {
+    if((window.pageYOffset || document.documentElement.scrollTop) > 500){
+        upBtn.classList.add('active');
+        upBtn.classList.remove('inactive');
+    } else if (!upBtn.classList.contains('inactive')){
+        upBtn.classList.add('inactive');
+        upBtn.classList.remove('active');
+    }
+});
+
+//--------------------------------------------------------------
+//                          /* Theming */
+//--------------------------------------------------------------
+const domain = 'http://localhost:63342';
+const styleSheets = Array.from(document.querySelectorAll('link'));
+const themeToggle = document.querySelector("#cb1");
+
+themeToggle.addEventListener('click', ()=> {
+    styleSheets.forEach(styleSheet => {
+        if(styleSheet.title){
+            styleSheet.disabled = !styleSheet.disabled;
+        }
+    });
+});
+
+const setCookie = ( cookieName, cookieValue,
+    lifespanInDays, validDomain ) => {
+    const domainString = validDomain ?
+        ("; domain=" + validDomain) : '' ;
+    document.cookie = cookieName +
+        "=" + encodeURIComponent( cookieValue ) +
+        "; max-age=" + 60 * 60 *
+        24 * lifespanInDays +
+        "; path=/" + domainString ;
+};
+
+const  switchStyle = ( cssTitle ) => {
+    styleSheets.forEach(styleSheet => {
+        if((styleSheet.title )&& (styleSheet.title === cssTitle)){
+            styleSheet.disabled = false;
+        } else if (styleSheet.title ) {
+            styleSheet.disabled = true;
+        }
+    });
+    setCookie('style',cssTitle,10,domain)
+};
+
+const getCookie = ( cookieName )  => {
+    const cookieString = document.cookie ;
+    console.log(cookieString);
+    if (cookieString.length !== 0) {
+        const cookieArray = cookieString.split( '; ' );
+        const cookieValue = cookieArray.filter(cookie => cookie.match ( cookieName + '=(.*)' ));
+        if (cookieValue != null) {
+            return decodeURIComponent (cookieValue[0]) ;
+        }
+    }
+    return '' ;
+};
+
+const setStyleFromCookie = () => {
+    const cssTitle = getCookie( 'style' );
+    console.log(cssTitle)
+    if (cssTitle.length) {
+        switchStyle(cssTitle);
+    }
+};
 
 
 
